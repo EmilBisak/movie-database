95990f990426ff1a65627bae4ce9c5229a0093f8
EmilBisak <you@example.com>
2018-11-08 04:49:11 +0100

    Updates

---
+++ precache-manifest.9d432a970bb45d65cc9231be120c4eaa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc7320d7b3bb6202f24e16f2a19c898f",
+    "url": "/movie-database/static/media/cinema.dc7320d7.png"
+  },
+  {
+    "revision": "12844d29d706708d5559",
+    "url": "/movie-database/static/js/runtime~main.12844d29.js"
+  },
+  {
+    "revision": "e0b9d587076f04103469",
+    "url": "/movie-database/static/js/main.e0b9d587.chunk.js"
+  },
+  {
+    "revision": "5e2571db2a59e31beaa4",
+    "url": "/movie-database/static/js/1.5e2571db.chunk.js"
+  },
+  {
+    "revision": "e0b9d587076f04103469",
+    "url": "/movie-database/static/css/main.6557f101.chunk.css"
+  },
+  {
+    "revision": "5e2571db2a59e31beaa4",
+    "url": "/movie-database/static/css/1.2e853026.chunk.css"
+  },
+  {
+    "revision": "7e7d0d0783ced822b3b1f0c9e7ebbe9f",
+    "url": "/movie-database/index.html"
+  }
+];
