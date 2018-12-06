a5e7d9e004bf7f72c1340aa59b8cc0d5cba150ff
EmilBisak <you@example.com>
2018-12-06 05:04:03 +0100

    Updates

---
+++ precache-manifest.8f0c6c8cce85c7a493b51be3d7f27096.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc7320d7b3bb6202f24e16f2a19c898f",
+    "url": "/movie-database/static/media/cinema.dc7320d7.png"
+  },
+  {
+    "revision": "12844d29d706708d5559",
+    "url": "/movie-database/static/js/runtime~main.12844d29.js"
+  },
+  {
+    "revision": "9d68ea9f27ab7986ab27",
+    "url": "/movie-database/static/js/main.9d68ea9f.chunk.js"
+  },
+  {
+    "revision": "de25db29fc44e3383bd5",
+    "url": "/movie-database/static/js/1.de25db29.chunk.js"
+  },
+  {
+    "revision": "9d68ea9f27ab7986ab27",
+    "url": "/movie-database/static/css/main.84464111.chunk.css"
+  },
+  {
+    "revision": "de25db29fc44e3383bd5",
+    "url": "/movie-database/static/css/1.dd87713f.chunk.css"
+  },
+  {
+    "revision": "15a9f72a907f772d1b3d1a04aa0e1d7d",
+    "url": "/movie-database/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/movie-database/precache-manifest.9d432a970bb45d65cc9231be120c4eaa.js"
+  "/movie-database/precache-manifest.8f0c6c8cce85c7a493b51be3d7f27096.js"
 );
 
 workbox.clientsClaim();
